1b9ce9fc0734c10eb988ae73aa6cd5cbd90f4d82
Jonathan De La Cruz <jonathandelacruzalvarez@gmail.com>
2024-11-20 15:49:56 -0600

    Se agregó la barra de niveles

--- lib/game/home.dart
+++ lib/game/home.dart
@@ -18,113 +18,145 @@ class HomePage extends StatelessWidget {
           return Center(child: Text('Error: ${snapshot.error}'));
         } else {
           return Scaffold(
-  body: Stack(
-    children: [
-      Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: snapshot.data!, // Set the first image as background
-            fit: BoxFit.cover,
-          ),
-        ),
-      ),
-      Positioned(
-        top: 250, // Adjust position as needed
-        right: -25,
-        child: Container(
-          width: 450, // Set the width of the second image
-          height: 450, // Set the height of the second image
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(Assets.conejo), // Your second image
-              fit: BoxFit.cover,
+            body: Stack(
+              children: [
+                // Fondo
+                Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: snapshot.data!,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                // Nivelador
+                Positioned(
+                  top: 20,
+                  left: 20,
+                  child: const LevelIndicator(),
+                ),
+                // Imágenes adicionales
+                ..._buildImages(),
+              ],
             ),
-          ),
-        ),
-      ),
-      Positioned(
-        top: 250, // Adjust position as needed
-        right: -25,
-        child: Container(
-          width: 450, // Set the width of the second image
-          height: 450, // Set the height of the second image
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(Assets.lente), // Your second image
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-      ),
-      Positioned(
-        top: 250, // Adjust position as needed
-        right: -25,
-        child: Container(
-          width: 450, // Set the width of the second image
-          height: 450, // Set the height of the second image
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(Assets.bigote), // Your second image
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-      ),
-      Positioned(
-        top: 250, // Adjust position as needed
-        right: -25,
-        child: Container(
-          width: 450, // Set the width of the second image
-          height: 450, // Set the height of the second image
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(Assets.sombrero), // Your second image
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-      ),
-      Positioned(
-        top: 250, // Adjust position as needed
-        right: -25,
-        child: Container(
-          width: 450, // Set the width of the second image
-          height: 450, // Set the height of the second image
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(Assets.bufanda), // Your second image
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-      ),
-      Positioned(
-        top: 250, // Adjust position as needed
-        right: -25,
-        child: Container(
-          width: 450, // Set the width of the second image
-          height: 450, // Set the height of the second image
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(Assets.zanahoria), // Your second image
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-      ),
-    ],
-  ),
-);
-
+          );
         }
       },
     );
   }
 
+  List<Widget> _buildImages() {
+    final images = [
+      Assets.conejo,
+      Assets.lente,
+      Assets.bigote,
+      Assets.sombrero,
+      Assets.bufanda,
+      Assets.zanahoria,
+    ];
+
+    return images
+        .asMap()
+        .entries
+        .map(
+          (entry) => Positioned(
+            top: 250 + (entry.key * 10), // Incremento para separación opcional
+            right: -25,
+            child: Container(
+              width: 450,
+              height: 450,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(entry.value),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        )
+        .toList();
+  }
+
   Future<ImageProvider> _loadBackgroundImage() async {
     final image = await Flame.images.load(Assets.fondo);
     return MemoryImage(
-      (await image.toByteData(format: ImageByteFormat.png))!.buffer.asUint8List(),
+      (await image.toByteData(format: ImageByteFormat.png))!
+          .buffer
+          .asUint8List(),
+    );
+  }
+}
+
+class LevelIndicator extends StatelessWidget {
+  const LevelIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color(0xFF53746E),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      height: 60,
+      width: 360,
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 20,
+            backgroundColor: Colors.grey[300],
+            child: const Icon(
+              Icons.person,
+              color: Colors.brown,
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "NIVEL 1",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "NIVEL 2",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 5),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: LinearProgressIndicator(
+                    value: 0.9,
+                    backgroundColor: Colors.grey[400],
+                    color: Colors.yellow[700],
+                    minHeight: 10,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 10),
+          const Icon(
+            Icons.notifications,
+            color: Colors.white,
+          ),
+        ],
+      ),
     );
   }
 }
